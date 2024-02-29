f9824ac573e484520a07ba03a0e3ed00e5195cde
Dilip8249 <dilipchoudhury690@gmail.com>
2024-02-29 18:18:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function getColor() {
+  const randomNumber = Math.floor(Math.random() * 16777215);
+  const randomCode = "#" + randomNumber.toString(16);
+  document.body.style.backgroundColor = randomCode;
+  document.getElementById("random-code").innerHTML = randomCode;
+}
+getColor();
